b07b5acc0844175c8f9383d886ccfc126656529b
vera <alvaro.touzon@gmail.com>
2017-11-07 22:49:18 +0100

    advanced use of tape

--- test/pagination.test.js
+++ test/pagination.test.js
@@ -1,11 +1,12 @@
 const test = require('tape'),
 	{spy} = require('sinon'),
-	{getPages, Pages, getTotalPages} = require('../lib/table/pagination'),
+	{getPages, Pages, getTotalPages, getButtonFirstPage} = require('../lib/table/pagination'),
 	tapSpec = require('tap-spec');
 
-test('When got 19 element, in page 1 and 5 pagesShown and 3 itemsPerPage', ({ok, end})=> {
+test('When got 19 element, when 5 pagesShown and 3 itemsPerPage', ({ok, end})=> {
 	let pages = getPages(19, 5, 3);
-	console.log(pages.length);
-	ok(pages.length===4, 'Got 4 pages')
+	ok(pages.length===4, 'Got 4 pages');
+	ok(getButtonFirstPage(19, 3, 5, 3).length===0, 'not show button goto first page when page is 3');
+	ok(getButtonFirstPage(19, 4, 5, 3).length===1, 'yes show button goto first page when page is 4');
   	end();
 });
