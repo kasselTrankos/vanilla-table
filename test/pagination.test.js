ac0d5aee3e065ba6d6019abd693f7da53b1ee0b5
vera <alvaro.touzon@gmail.com>
2017-11-08 20:21:42 +0100

    clean table

--- test/pagination.test.js
+++ test/pagination.test.js
@@ -1,6 +1,6 @@
 const test = require('tape'),
 	{spy} = require('sinon'),
-	{getPages, Pages, getTotalPages, getButtonFirstPage, isInLastMiddle} = require('../lib/table/pagination'),
+	{getPages, getFirstPage,Pages, getTotalPages, getButtonFirstPage, isInLastMiddle} = require('../lib/table/pagination'),
 	tapSpec = require('tap-spec');
 
 test('When got 19 element, 5 pagesShown and 3 itemsPerPage', ({ok, end})=> {
@@ -9,6 +9,9 @@ test('When got 19 element, 5 pagesShown and 3 itemsPerPage', ({ok, end})=> {
 	ok(isInLastMiddle(19, 7, 5, 3)===true , ' When is page 7 is  at the end of pagination')
 	ok(getButtonFirstPage(19, 3, 5, 3).length===0, 'not show button goto first page when page is 3');
 	ok(getButtonFirstPage(19, 4, 5, 3).length===1, 'yes show button goto first page when page is 4');
+	ok(getFirstPage(19, 3, 5, 3)===1 , 'page 1 is first when is in page 3');
+	ok(getFirstPage(40, 6, 5, 3)===4 , 'page 4 is first when is in page 6');
+	ok(getFirstPage(50, 6, 9, 3)===2 , 'page 2 is first when is in page 6 and pagesShown is 9');
 
   	end();
 });
