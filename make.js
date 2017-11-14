9e84af27d97ca4090922a896ea2727650267f65a
vera <alvaro.touzon@gmail.com>
2017-11-14 22:45:52 +0100

    joer con 2e2 and travis, dificil de mantener

--- make.js
+++ make.js
@@ -4,7 +4,7 @@ const es2016 = execFile('node', ['es2016'], (error, stdout, stderr) => {
     if (error) {
         // console.error('stderr', stderr);
         // exec('kill -9 $(lsof -t -i:9001 -sTCP:LISTEN)');
-        spawn("sh", ["-c", "kill -INT -"+es2016.pid]);
+        // spawn("sh", ["-c", "kill -INT -"+es2016.pid]);
         // throw error;
     }
 });
@@ -16,7 +16,7 @@ es2016.stdout.on('data', function(data) {
 const startServer = () => {
 	const static_server = execFile('node', ['static_server'], (error, stdout, stderr) => {
 	    if (error) {
-	    	spawn("sh", ["-c", "kill -INT -"+static_server.pid]);
+	    	// spawn("sh", ["-c", "kill -INT -"+static_server.pid]);
 	        // console.error('stderr', stderr.toString(), 'opo');
 
 	        // throw error;
