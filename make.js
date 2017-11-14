64492262e562b955b5493c3ec53e14a71816281e
vera <alvaro.touzon@gmail.com>
2017-11-14 22:00:34 +0100

    kill process , only byu linux

---
+++ make.js
@@ -0,0 +1,29 @@
+const execFile = require('child_process').execFile,
+	spawn = require('child_process').spawn;
+const es2016 = execFile('node', ['es2016'], (error, stdout, stderr) => {
+    if (error) {
+        // console.error('stderr', stderr);
+        // exec('kill -9 $(lsof -t -i:9001 -sTCP:LISTEN)');
+        spawn("sh", ["-c", "kill -INT -"+es2016.pid]);
+        // throw error;
+    }
+});
+es2016.stdout.on('data', function(data) {
+    console.log(`[1] transpile: ${data.toString()}`); 
+    startServer();
+
+});
+const startServer = () => {
+	const static_server = execFile('node', ['static_server'], (error, stdout, stderr) => {
+	    if (error) {
+	    	spawn("sh", ["-c", "kill -INT -"+static_server.pid]);
+	        // console.error('stderr', stderr.toString(), 'opo');
+
+	        // throw error;
+	    }
+	});
+	static_server.stdout.on('data', function(data) {
+		pid  =static_server.pid;
+	    console.log(`[2] sever: ${data.toString()}`); 
+	});
+};
--- test/pagination.e2e.js
+++ test/pagination.e2e.js
@@ -1,10 +1,10 @@
 import { Selector, ClientFunction } from 'testcafe';
 
-fixture `Getting Started`
+fixture `Load localhost at port 9001`
     .page `http://localhost:9001`;
 
 const _a = Selector('#pagination ul li:nth-child(3) a');
-test('When clicke in 3 link goto 3 page and change url path', async t => {
+test('When clicke in 3 link goto 3 page and change url path with pathname === /3', async t => {
    	await t
         .click(_a)
         .wait(1200);
