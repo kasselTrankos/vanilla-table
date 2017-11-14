5d48555b247f2ad9d63232b4a0668229c1a0abb6
vera <alvaro.touzon@gmail.com>
2017-11-14 22:53:11 +0100

    more clean code, hard work

--- es2016.js
+++ es2016.js
@@ -8,15 +8,15 @@ var b = browserify({
 	  cache: {},
   	debug: true,
   	packageCache: {},
-});
+})
 .transform(babelify, {presets: ["es2016"], plugins: [ "transform-object-rest-spread", ["transform-class-properties", { "spec": true }]]});
 b.on('update', bundle)
 .bundle()
 .pipe(fs.createWriteStream("build.js"));
-console.debug(`Created code at ${new Date().toISOString()}`);
+console.info(`Created code at ${new Date().toISOString()}`);
 function bundle(err){
     if(err) {
-      console.debug(`Error ${err} at ${new Date().toISOString()}`);  
+      console.info(`Error ${err} at ${new Date().toISOString()}`);  
       return false
     }
   	console.debug(`Updated code at ${new Date().toISOString()}`);
