e4b20bdb9c62fd2fcdae40fc1952d792a9a7c125
vera <alvaro.touzon@gmail.com>
2017-11-14 21:11:36 +0100

    travis+testcafe, will works

--- static_server.js
+++ static_server.js
@@ -1,9 +1,10 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+const http = require('http'),
+      url = require('url'),
+      fs = require('fs'),
+      path = require('path');
 // you can pass the parameter in the command line. e.g. node static_server.js 3000
 const port = process.argv[2] || 9001;
+
 http.createServer(function (req, res) {
   console.log(`${req.method} ${req.url}`);
   // parse URL
@@ -27,32 +28,25 @@ http.createServer(function (req, res) {
     '.eot': 'appliaction/vnd.ms-fontobject',
     '.ttf': 'aplication/font-sfnt'
   };
+
   fs.exists(pathname, function (exist) {
     if(!exist) {
       pathname = '.';
-      //var e = exist ? 'it\'s there' : 'no exists!';
-      // if the file is not found, return 404
-      //res.statusCode = 404;
-      //res.end(`File ${pathname} not found! ${e}`);
-      //return;
     }
-    // if is a directory, then look for index.html
     if (fs.statSync(pathname).isDirectory()) {
       pathname += '/index.html';
     }
-    // read file from file system
     fs.readFile(pathname, function(err, data){
       if(err){
         res.statusCode = 500;
         res.end(`Error getting the file: ${err}.`);
       } else {
-        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
         const ext = path.parse(pathname).ext;
-        // if the file is found, set Content-type and send data
         res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
         res.end(data);
       }
     });
   });
+  
 }).listen(parseInt(port));
 console.log(`Server listening on port ${port}`);
